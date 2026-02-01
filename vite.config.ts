2303af160532eb19dde2c83422f1ff006a914437
Fourloubey <gaetan.fourloubey@gmail.com>
2026-02-01 14:54:06 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   base: "/mon-site-react/",
-  // ...le reste
-})
   server: {
-    host: "::",
+    host: "0.0.0.0",
     port: 8080,
     hmr: {
       overlay: false,
